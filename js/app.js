12bc72a02f8f624c68f61ff01c0f86c10657b1c7
donny <donny.vallejo@gmail.com>
2017-04-03 09:42:48 -0700

    did horrible

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+//
 
+     $( ".squares" ).eq("li").each();
+
+  var squares = $(".squares");
+  //var counter = 1;
+//  var playerATurn = true;
+
+  console.log(".squares", squares);
+  // check if counter has a remainder of 0 when % 2
+  // if ( counter % 2 !== 0) {
+  //   // if it does
+  //   // player b's turn
+  //   playerATurn = false;
+  //   // else
+  // } else {
+  //   // player a's turn
+  //   playerATurn = true;
+  // };
+  //
+  // determine winner
+
+  $(".squares").on("click", function(){
+    console.log("click works");
+  });
+//  console.log("End of document.ready in app.js");
 });
+//  console.log("End of app.js");
